f7d903f5393c3243736f6b9e4c376443fdfb0657
ArtemBagryantsev <artembagrjancev@gmail.com>
2022-10-12 20:27:12 +1100

    Initial commit

---
+++ DadJoke_picker_app/DadJoke Picker app.py
@@ -0,0 +1,31 @@
+import requests
+from random import choice
+from pyfiglet import figlet_format
+from termcolor import colored
+
+header = figlet_format('DAD JOKE 2022')
+header = colored(header, color = 'magenta')
+print(header)
+
+user_input = input('What would you like to search for? ')
+url = 'https://icanhazdadjoke.com/search'
+res = response = requests.get(
+    url,
+    headers = {'Accept': 'application/json'},
+    params = {
+        'term': user_input
+    }
+).json()
+
+num_jokes = res['total_jokes']
+results = res['results']
+
+if num_jokes > 1:
+    print(f'There are {num_jokes} jokes about "{user_input}". Here`s one: ')
+    print(choice(results)['joke'])
+elif num_jokes == 1:
+    print(f'There is only one joke about "{user_input}"')
+    print(results['joke'])
+else:
+    print(f'Sorry! There are NO jokes with your term "{user_input}"')
+
